Fixed robot offset and quaternion order bug

policies/apf_policy.py
@@ -34,17 +34,20 @@ class APFPolicy(Policy):
     def predict(self, obs: Vector) -> (Vector, InfoVector):
         action = np.zeros(8)
         [rl_action], _ = self.rl_policy.predict(obs)
-        rl_goal_pos = rl_action[:3]
-        rl_goal_rot = rl_action[3:7]
+        rl_goal_pos = np.array([1.3, 1, 0.6], dtype=np.float32)  # rl_action[:3]
+        [qw, qx, qy, qz] = [0, 1, 0, 0]  # rl_action[3:7]
+        rl_goal_rot = np.array([qx, qy, qz, qw], dtype=np.float32)
         ob = obs[0]["observation"]
         theta = ob[7:14]
 
         obstacle_attributes = np.array([], dtype=np.float64)  # TODO add obstacles
 
+        rl_goal_pos -= np.array([0.8, 0.75, 0.42], dtype=np.float32)
         forces = control_step(theta, rl_goal_pos, rl_goal_rot, obstacle_attributes)
         max_forces = self.envs[0].sim.model.actuator_forcerange[:7, 1]
         action[:7] = forces / max_forces  # Normalize forces to [-1, 1]
 
         action[7] = rl_action[7]  # Directly use RL gripper action
+        print("action: {}".format(action))
 
         return [action], _